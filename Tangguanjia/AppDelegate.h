3e1d77560322653ff4128c69f1403b57687a6b2c
huoyj <huoyj@huoyjdeMacBook-Pro.local>
2015-07-09 17:10:10 +0800

    Initial Commit

---
+++ Tangguanjia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tangguanjia
+//
+//  Created by huoyj on 15/7/9.
+//  Copyright (c) 2015年 huoyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tangguanjia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tangguanjia
+//
+//  Created by huoyj on 15/7/9.
+//  Copyright (c) 2015年 huoyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
